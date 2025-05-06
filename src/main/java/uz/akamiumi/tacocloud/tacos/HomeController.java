0c146069bce16130e8274b4a47cad5fb93bc4c56
AkamiUmi <akamiumi@gmail.com>
2025-05-06 23:01:06 +0500

    Spring In Action - Taco Cloud

---
+++ src/main/java/uz/akamiumi/tacocloud/tacos/HomeController.java
@@ -0,0 +1,12 @@
+package uz.akamiumi.tacocloud.tacos;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class HomeController {
+  @GetMapping("/")
+  public String hone() {
+    return "home";
+  }
+}
---
+++ src/main/java/uz/akamiumi/tacocloud/tacos/Taco.java
@@ -0,0 +1,11 @@
+package uz.akamiumi.tacocloud.tacos;
+
+import lombok.Data;
+
+import java.util.List;
+
+@Data
+public class Taco {
+  private String name;
+  private List<Ingredient> ingredients;
+}
